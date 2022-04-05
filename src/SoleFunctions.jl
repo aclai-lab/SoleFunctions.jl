8f9c26976d6d8209d347e0758d2ca1d6dd72c35d
mauro-milella <milella_mauro@live.it>
2022-04-05 15:37:11 +0200

    get_desc function

--- src/SoleFunctions.jl
+++ src/SoleFunctions.jl
@@ -3,6 +3,11 @@ module SoleFunctions
 using Statistics
 using Catch22
 
+# -------------------------------------------------------------
+# exports
+export get_desc
+
+# taken from https://github.com/aclai-lab/SoleBase.jl/blob/dev/src/data/dataset/describe.jl
 const desc_dict = Dict{Symbol,Function}(
     :mean => mean,
     :min => minimum,
@@ -18,4 +23,35 @@ const auto_desc_by_dim = Dict{Integer,Vector{Symbol}}(
     1 => [:mean, :min, :max, :quantile_1, :median, :quantile_3]
 )
 
+""" 
+    get_desc(descfun, values)
+
+Apply functions represented by `descfun` to `values`.\n
+Return a dictionary containing the associations :symbol -> value (e.g :mean -> 3.5)
+
+## PARAMETERS
+* `descfun` is a `Vector` of Symbols which are mapped to specific functions.
+* `values` are a `Vector` of Numbers on which the description functions are applied.
+
+## EXAMPLE
+```julia-repl 
+julia> desc_syms = [:mean, :min, :max]
+julia> values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+julia> values_description = get_desc(desc_syms, values)
+Dict{Symbol, Real}(:max => 10, :mean => 5.5, :min => 1)
+```
+"""
+function get_desc(descfun::Vector{Symbol}, values::Vector{<:Number})
+    # a symbol is valid only if it is defined in desc_dict
+    for df in descfun
+        @assert df in keys(desc_dict) "`$(df)` is not a valid descriptor"
+    end
+
+    ans_dict = Dict{Symbol, Real}()
+    apply(f, x) = f(x)
+    [push!(ans_dict, df => apply(desc_dict[df], values)) for df in descfun]
+
+    return ans_dict
 end
+
+end # module
