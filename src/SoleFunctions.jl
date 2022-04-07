249acdfaa88d967ab756f97f1e58b89f31b4713d
mauro-milella <milella_mauro@live.it>
2022-04-07 23:23:06 +0200

    apply_descriptors (general version) and testing

--- src/SoleFunctions.jl
+++ src/SoleFunctions.jl
@@ -5,9 +5,9 @@ using Catch22
 
 # -------------------------------------------------------------
 # exports
-export get_desc
+export apply_descriptors
 
-# taken from https://github.com/aclai-lab/SoleBase.jl/blob/dev/src/data/dataset/describe.jl
+# function dictionary
 const desc_dict = Dict{Symbol,Function}(
     :mean => mean,
     :min => minimum,
@@ -19,39 +19,80 @@ const desc_dict = Dict{Symbol,Function}(
     (getnames(catch22) .=> catch22)...
 )
 
-const auto_desc_by_dim = Dict{Integer,Vector{Symbol}}(
-    1 => [:mean, :min, :max, :quantile_1, :median, :quantile_3]
+# default functions by dimension
+const dim_desc = Dict{Integer,Vector{Symbol}}(
+    # TODO add default functions for other dimensions
+    0 => [:mean, :min, :max],
+    1 => [:min, :max, :quantile_1, :median, :quantile_3],
+    2 => [:mean, :min, :max, :quantile_1, :median, :quantile_3]
 )
 
 """ 
-    get_desc(descfun, values)
+    apply_descriptors(descriptors, values)
 
-Evaluate `descfun` with `values`.\n
-Return a dictionary containing the associations :symbol -> value (e.g :mean -> 3.5)
+Evaluate `descriptors` with `values`.\n
+Return a dictionary containing the associations descriptor -> value
 
 ## PARAMETERS
-* `descfun` is a `Vector` of Symbols which are mapped to specific functions.
+* `descriptors` is a `Vector` containing both Symbols and Functions.
 * `values` is a `Vector` of Numbers on which the description functions are applied.
 
 ## EXAMPLE
 ```julia-repl 
-julia> desc_syms = [:mean, :min, :max]
+julia> desc_syms = [:mean, :min, maximum]
 julia> values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-julia> values_description = get_desc(desc_syms, values)
-Dict{Symbol, Real}(:max => 10, :mean => 5.5, :min => 1)
+julia> values_description = descriptors(desc_syms, values)
+Dict{Symbol, Real}(:max => 10, :mean => 5.5, :min => 1)\n
 ```
+
+    apply_descriptors(values)
+Depending on `values` dimension, apply default functions.
+
+# TODO - show to the user which functions are applied
+
+## PARAMETERS
+* `values` is a `Vector` of Numbers on which the description functions are applied.
+\n
 """
-function get_desc(descfun::Vector{Symbol}, values::Vector{<:Number})
-    # a symbol is valid only if it is defined in desc_dict
-    for df in descfun
-        @assert df in keys(desc_dict) "`$(df)` is not a valid descriptor"
+function apply_descriptors(descriptors::Vector{Any},  values::Array{<:Number})
+    # descriptors must contain only Function and Symbol types
+    try
+        convert(Vector{Union{Function, Symbol}}, descriptors)
+    catch e
+        @error "Argument descriptors must contain only Function and Symbol types"
     end
 
-    ans_dict = Dict{Symbol, Real}()
-    apply(f, x) = f(x)
-    [push!(ans_dict, df => apply(desc_dict[df], values)) for df in descfun]
+    # returned dictionary
+    answer = Dict{Union{Symbol, Function}, Real}()
 
-    return ans_dict
+    # all description functions are applied, results are stored in a new "answer" entry
+    for desc in descriptors
+        if isa(desc, Symbol)
+            push!(answer, desc => desc_dict[desc](values))
+        else
+            push!(answer, desc => desc(values))
+        end
+    end
+
+    return answer
+end
+
+function apply_descriptors(descriptors::Vector{Symbol}, values::Array{<:Number})
+    answer = Dict{Symbol, Real}()
+    [push!(answer, desc => desc_dict[desc](values)) for desc in descriptors]
+    return answer
+end
+
+function apply_descriptors(descriptor::Symbol, values::Array{<:Number})
+    return apply_descriptors( [descriptor] , values)
+end
+
+function apply_descriptors(values::Array{<:Number})
+    dims = ndims(values)
+    return apply_descriptors( dim_desc[dims] , values)
 end
 
 end # module
+
+#da scrivere nella pull request:
+#bisogna stare attenti ai tipi in apply descriptors
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,26 @@ using SoleFunctions
 using Test
 
 @testset "SoleFunctions.jl" begin
-    # Write your tests here.
+
+    test1 = [5,3,7,9,10,921.231,1,32,1323,12.3,12.2,212]
+    min = minimum(test1)
+    max = maximum(test1)
+    mean = mean(test1)
+    median = median(test1)
+    q_1 = quantile(test1, 0.25)
+    q_3 = quantile(test1, 0.75)
+    desc1 = Dict{Symbol, Real}(:min => min, :max => max, :mean => mean, :median => median, :quantile_1 => q_1, :quantile_3 => q_3)
+    
+    # general test (both Symbols and Functions)
+    @test_throws MethodError apply_descriptors( ["minimum", minimum, :min], [1,2,3])
+
+    # single Symbol test
+
+    # single Function test
+
+    # default test
+
+    #@test apply_descriptors(test1) == desc1
+
+
 end
