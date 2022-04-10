3abecb66a65e6b2ed6512e4183332c336261d663
mauro-milella <milella_mauro@live.it>
2022-04-10 16:57:55 +0200

    test commenting

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,7 +9,9 @@ rng = MersenneTwister(123)
     using Statistics
     using Catch22
 
+    # input in one-dimensional case
     one_dim = rand(Float64,100)
+    # symbol and function arrays used in the test
     sym_one = [:max,:mean]
     func_one = [minimum]
 
@@ -17,29 +19,7 @@ rng = MersenneTwister(123)
     sym_two = [:max]
     func_two = [minimum,mean]
 
-    @testset "values, symbols, functions test" begin
-
-        ans_t1d1 = Dict{Union{Symbol,Function},Number}(
-            :max => maximum(one_dim),
-            :mean => mean(one_dim),
-            minimum => minimum(one_dim)
-        )
-
-        @test_throws UndefVarError apply_descriptors(one_dim,sym_one,[nonexistent_function])
-        @test apply_descriptors(one_dim,sym_one,func_one) == ans_t1d1
-        @test apply_descriptors(one_dim,func_one,sym_one) == ans_t1d1
-
-        ans_t1d2 = Dict{Union{Symbol,Function},Number}(
-            :max => maximum(two_dim),
-            minimum => minimum(two_dim),
-            mean => mean(two_dim)
-        )
-
-        @test apply_descriptors(two_dim,sym_two,func_two) == ans_t1d2
-        @test apply_descriptors(two_dim,func_two,sym_two) == ans_t1d2
-
-    end
-
+    # all functions are tested for a certain dimension
     @testset "values only test" begin
 
         ans_t2d1 = Dict{Union{Symbol,Function},Number}(
@@ -61,6 +41,29 @@ rng = MersenneTwister(123)
 
         @test apply_descriptors(one_dim) == ans_t2d1
         @test apply_descriptors(two_dim) == ans_t2d2
+
+    end
+
+    @testset "values, symbols, functions test" begin
+
+        ans_t1d1 = Dict{Union{Symbol,Function},Number}(
+            :max => maximum(one_dim),
+            :mean => mean(one_dim),
+            minimum => minimum(one_dim)
+        )
+
+        @test_throws UndefVarError apply_descriptors(one_dim,sym_one,[nonexistent_function])
+        @test apply_descriptors(one_dim,sym_one,func_one) == ans_t1d1
+        @test apply_descriptors(one_dim,func_one,sym_one) == ans_t1d1
+
+        ans_t1d2 = Dict{Union{Symbol,Function},Number}(
+            :max => maximum(two_dim),
+            minimum => minimum(two_dim),
+            mean => mean(two_dim)
+        )
+
+        @test apply_descriptors(two_dim,sym_two,func_two) == ans_t1d2
+        @test apply_descriptors(two_dim,func_two,sym_two) == ans_t1d2
 
     end
 
